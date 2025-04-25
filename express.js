62fa597c2e21ae5eb56d47fc6019850a16ac6b91
eyop <eyobketema8@gmail.com>
2025-04-25 18:01:25 +0300

    feat: enhance server startup message and add /api route

--- express.js
+++ express.js
@@ -7,10 +7,15 @@ import gistCard from "./api/gist.js";
 import express from "express";
 
 const app = express();
-app.listen(process.env.port || 9000);
+app.listen(process.env.port || 9000, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.port || 9000}`,
+  );
+});
 
 app.get("/", statsCard);
 app.get("/pin", repoCard);
 app.get("/top-langs", langCard);
 app.get("/wakatime", wakatimeCard);
 app.get("/gist", gistCard);
+app.get("/api", statsCard);
